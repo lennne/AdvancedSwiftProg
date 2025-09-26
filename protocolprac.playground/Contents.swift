5cf1117cd569add21b04dd5fae45ea04fd53e8a9
lennne <kobiansah@gmail.com>
2025-09-26 01:15:01 -0230

    update: implemented defer

--- protocolprac.playground/Contents.swift
+++ protocolprac.playground/Contents.swift
@@ -220,3 +220,70 @@ enum CalculatorError: Error {
 }
 
 
+//But did you know that when an error is thrown on a particular line, any code after that line will not be executed?
+//Now, what can you do to remedy that? In this reading, you will learn to use a defer statement to execute code in a given code block, no matter what happens.
+
+//A defer statement executes specified statements before the code execution leaves the current block of code.
+//The statements provided inside the defer block are executed at the end of a scope, like a function.
+//They are executed every time before exiting the scope, even if an error is thrown.
+
+//Note that the defer block is only executed when the current scope is left, which may not be the same as the return from the function.
+
+//The syntax of the defer statement
+//A defer statement is declared inside the scope of a given code block, for example, inside a body of a function
+func example() {
+    defer {
+        print("This is the last message")
+    }
+    
+    print("This is the first message")
+}
+
+//multiple defer blocks
+//If multiple defer statements are defined in a single scope, the order they are defined is the reverse of the order they are executed in.
+//This means that the first defer block defined will be executed last.
+
+func printTheNumbers (){
+    defer {print("1")}
+    defer {print("2")}
+    defer {print("3")}
+    
+    print("4")
+}
+
+printTheNumbers()
+
+// USING DEFER WITH "DO CATCH"
+//It is sometimes necessary to execute certain code even if an error is thrown. For example, suppose you need to open and read a file’s content.
+//let file= open(“file.txt”)
+//
+//
+//do {
+//    let result = try readFile(file)
+//    
+//    file.close()
+//} catch {
+//    file.close()
+//}
+
+
+// The text file is opened and initialized to a file constant.
+// Then the throwable function readFile is called to read its contents inside the do block.
+// Notice that in the example, the close function is called in two places –
+// first after the successful read of the file, and
+// second – inside the catch block in case there is an error and the do block does not complete code execution.
+// As repeated code in software development could lead to confusion, it should be minimal.
+
+//This can be optimized using a defer statement:
+//let file= open(“file.txt”)
+//
+//
+//do {
+//    defer {
+//        file.close()
+//    }
+// 
+//    let result = try readFile(file)
+//} catch {
+//    // Handle errors here
+//}
